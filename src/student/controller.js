15e9b08da85ff1b8390d7c0e55df5eb9480c750a
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-19 23:20:43 +0500

    implemented getStudentById

--- src/student/controller.js
+++ src/student/controller.js
@@ -11,7 +11,19 @@ const getStudents = (req, res) =>{
     })
 }
 
+// controller to get user by ID
+const getStudentById = (req, res) => {
+    const id = parseInt(req.params.id)
+    // Query to get data from PostgreSQL
+    pool.query(queries.getStudentById, [id] , (error, results)=>{
+        if(error) throw error;
+
+        res.status(200).json(results.rows)
+    })
+}
+
 
 module.exports = {
     getStudents,
+    getStudentById
 }
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,6 +4,7 @@ const router = Router()
 
 
 router.get('/' , controller.getStudents);
+router.get('/:id' , controller.getStudentById);
 
 module.exports = router;
 
